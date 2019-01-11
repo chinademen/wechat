5e2b95dbfa42c0c7bc77d96a3aeb19f893432554
chinademen <chinademen@gmail.com>
2019-01-12 01:07:53 +0800

    11

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,31 +1,6 @@
 
 Page({
   data: {
-    ratio: 102 / 152,
-    originUrl: '',
-    cropperResult: "../../image/main/zheng.png",
-    currentPage:1 
+    currentPage:2, 
   },
-  uploadTap() {
-    let _this = this
-    wx.chooseImage({
-      count: 1, // 默认9
-      sizeType: ['original'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      success(res) {
-        _this.setData({
-          originUrl: res.tempFilePaths[0],
-          cropperResult: ''
-        })
-      }
-    })
-  },
-  getCropperImg(e) {
-    if (e.detail.url) {
-      this.setData({
-        originUrl: '',
-        cropperResult: e.detail.url
-      })
-    }
-  }
 })
--- pages/takePicture/takePicture.js
+++ pages/takePicture/takePicture.js
@@ -7,10 +7,34 @@ Component({
       }
     }
   },
-  data:{
-
+  data: {
+    ratio: 102 / 152,
+    originUrl: '',
+    cropperResult: "../../image/main/zheng.png",
+    currentPage: 2
   },
-  methods:{
-
+  methods: {
+    uploadTap() {
+      let _this = this
+      wx.chooseImage({
+        count: 1, // 默认9
+        sizeType: ['original'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success(res) {
+          _this.setData({
+            originUrl: res.tempFilePaths[0],
+            cropperResult: ''
+          })
+        }
+      })
+    },
+    getCropperImg(e) {
+      if (e.detail.url) {
+        this.setData({
+          originUrl: '',
+          cropperResult: e.detail.url
+        })
+      }
+    }
   }
 })
